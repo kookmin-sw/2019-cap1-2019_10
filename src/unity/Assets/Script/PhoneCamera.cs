1af9052daa626354fd0d903f114f6d920cdc7441
jeunna <xjeunna@naver.com>
2019-04-06 01:20:30 +0900

    unity files upload
    1. setting splash image
    2. setting front camera to use
    3. upload README

---
+++ src/unity/Assets/Script/PhoneCamera.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhoneCamera : MonoBehaviour
+{
+    private bool camAvailable;
+    private WebCamTexture frontCam;
+    private Texture defaultBackground;
+    private int cameraPermission = 0;
+
+    public RawImage background;
+    public AspectRatioFitter fit;
+
+    Vector3 localScale = new Vector3(1f, 1f, 1f);
+    Vector3 localOrient = new Vector3(0, 0, 0);
+
+    private void Awake()
+    {
+        WebCamDevice[] devices = WebCamTexture.devices;
+    }
+
+    private void Start()
+    {
+        // permission 체크
+        if(!Application.HasUserAuthorization(UserAuthorization.WebCam))
+        {
+            Application.HasUserAuthorization(UserAuthorization.WebCam);
+        }
+        else
+        {
+            cameraPermission = 1;
+        }
+
+        while(cameraPermission != 1)
+        {
+            System.Threading.Thread.Sleep(1000);
+            if(Application.HasUserAuthorization(UserAuthorization.WebCam))
+            {
+                cameraPermission = 1;
+            }
+        }
+
+        defaultBackground = background.texture;
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        // 사용할 수 있는 카메라를 못찾음
+        if(devices.Length == 0)
+        {
+            Debug.Log("No camera detected");
+            camAvailable = false;
+            return;
+        }
+
+        // 사용할 수 있는 카메라 중에 전면 카메라를 사용하고 싶다
+        for(int i = 0; i < devices.Length; i++)
+        {
+            if(devices[i].isFrontFacing)
+            {
+                frontCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+            }
+        }
+
+        if(frontCam == null)
+        {
+            Debug.Log("Unable to find back camera");
+            return;
+        }
+
+        // 카메라 사용
+        // 근데 처음에 스타트하고 퍼미션 받고 돌아야하는데 스타트에 들어있어서 퍼미션이 바로 적용이 안됨
+        frontCam.Play();
+
+        // 이건 그냥 디버깅용으로 안보이니까 카메라 잘 떴는지 확인할려고 UI 텍스쳐로 띄워줌 카메라
+        background.texture = frontCam;
+
+        camAvailable = true;
+    }
+
+    private void Update()
+    {
+        if (!camAvailable)
+            return;
+
+        // 카메라 스케일이랑 각도를 내가 핸드폰을 돌릴때마다 맞춰서 나올 수 있도록 조정
+        float ratio = (float)frontCam.width / (float)frontCam.height;
+        fit.aspectRatio = ratio;
+
+        float scaleY = frontCam.videoVerticallyMirrored ? -1f : 1f;
+        //background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+        localScale.Set(1f, scaleY, 1f);
+        background.rectTransform.localScale = localScale;
+
+        int orient = -frontCam.videoRotationAngle;
+        //background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+        localOrient.Set(0, 0, orient);
+        background.rectTransform.localEulerAngles = localOrient;
+    }
+}
---
+++ src/unity/README.md
@@ -0,0 +1,20 @@
+어플만 실행해보고 싶다! 
+>  .apk 다운로드 후 핸드폰에서 실행
+<br/>
+
+유니티로 실행 : 
+```
+1. 유니티에서 New 파일을 하나 생성
+2. clone한 unity폴더의 Asset, Packages, ProjectSettings 폴더들을 덮어쓰기
+3. Scene에서 Start Scene 더블클릭
+4. 재생버튼을 눌러보면 해당 노트북에서 실행해볼 수 있음
+```
+<br/>
+
+유니티에서 안드로이드로 빌드하려면
+1. Edit > Perferences > External Tools 에서 SDK 세팅
+2. File > Build Settings 에서 Android Build로
+<br/>
+
+안되면 Build Settings에서 Player Settings에
+Other Settings에서 Package Name을 다른 거로 바꿔준다
