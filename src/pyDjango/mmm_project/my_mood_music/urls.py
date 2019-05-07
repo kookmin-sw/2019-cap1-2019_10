f80aa3627d690074188e912379164b8d157dd659
daeng325 <yuj325@naver.com>
2019-05-08 01:13:49 +0900

    modify urls

--- src/pyDjango/mmm_project/my_mood_music/urls.py
+++ src/pyDjango/mmm_project/my_mood_music/urls.py
@@ -1,15 +1,17 @@
 from django.conf.urls import include, url
 from rest_framework.urlpatterns import format_suffix_patterns
-from . import views
+from .views import *
 
 
-from django.urls import path, include
+from django.urls import path
 
 
 urlpatterns = format_suffix_patterns([
 	path('auth/', include('rest_framework.urls', namespace='rest_framework')),
-	path('', views.requestFaceAPI, name='show_table'),
+	path('', requestFaceAPI, name='show_table'),
 	# path('emotion_analysis', views.requestFaceAPI, name ='api_response'),
-	path('emotions/', views.EmotionList.as_view()),
-	url(r'emotions/(?P<pk>[0-9]+)/$', views.EmotionDetail.as_view()),
+	path('emotions/', EmotionList.as_view()),
+	path('emotions/<int:pk>/', EmotionDetail.as_view()),
+
 ])
+
