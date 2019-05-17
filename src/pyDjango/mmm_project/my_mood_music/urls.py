c6e993cf4176316f35c7a6e54bd725b0da1e54da
daeng325 <yuj325@naver.com>
2019-05-17 23:25:33 +0900

    set login & gettoken func

--- src/pyDjango/mmm_project/my_mood_music/urls.py
+++ src/pyDjango/mmm_project/my_mood_music/urls.py
@@ -2,15 +2,14 @@ from django.conf.urls import include, url
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import *
 
-
 from django.urls import path
 
-
 urlpatterns = format_suffix_patterns([
-	path('auth/', include('rest_framework.urls', namespace='rest_framework')),
-	path('', requestFaceAPI.as_view(), name='Microsoft Face API'),
-	# path('emotion_analysis', views.requestFaceAPI, name ='api_response'),
-	path('emotions/', EmotionList.as_view()),
-	path('emotions/<int:pk>/', EmotionDetail.as_view()),
+    path('auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', requestFaceAPI.as_view(), name='Microsoft Face API'),
+    path('speech/', call.as_view(), name='speech_to_emotion'),
+    path('api/user', UserAPI.as_view()),
+    path('api/getauthtoken', GetAuthToken.as_view()),
+    path('emotions/', EmotionList.as_view()),
+    path('emotions/<int:pk>/', EmotionDetail.as_view()),
 ])
-
--- src/pyDjango/mmm_project/my_mood_music/views.py
+++ src/pyDjango/mmm_project/my_mood_music/views.py
@@ -38,6 +38,8 @@ from rest_framework import generics
 from django.http.response import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import cognitive_face as CF
+from rest_framework.authtoken.models import Token
+
 
 # # viewSet classes
 # class UserViewSet(viewsets.ModelViewSet):
@@ -175,6 +177,91 @@ def show_table(request):
         result_str += '<p>' + i.emotion_name
 
     return HttpResponse(result_str)
+
+class UserAPI(DestroyAPIView, CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = CreateUserSerializer
+
+    def perform_destroy(self, instance):
+        user = User.objects.get(username=self.request.data['username'], email=self.request.data['email'])
+        if user.check_password(self.request.data['password']) is False:
+            return Response('You are not authorized to do that.', status=status.HTTP_401_UNAUTHORIZED)
+        instance.delete()
+
+class GetAuthToken(GenericAPIView):
+    throttle_classes = ()
+    permission_classes = ()
+    parser_classes = (parsers.FormParser, parsers.MultiPartParser, parsers.JSONParser,)
+    renderer_classes = (renderers.JSONRenderer,)
+
+    def post(self, request):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
+
+
+
+from keras.utils import np_utils
+from sklearn.preprocessing import LabelEncoder
+import librosa
+import numpy as np
+import pandas as pd
+import glob
+import tensorflow as tf
+from keras.models import model_from_json
+
+
+class call(APIView):
+
+    def post(self, request, format=None):
+        file = request.POST.get('my file data')
+        print(request.headers)
+        return HttpResponse(labelfrommodel(file))
+
+    def get(self, request, format=None):
+        return HttpResponse("HHH")
+
+
+lb = LabelEncoder()
+label = [
+    "angry",
+    "calm",
+    "fearful",
+    "happy",
+    "sad",
+    "angry",
+    "calm",
+    "fearful",
+    "happy",
+    "sad"
+]
+
+
+def labelfrommodel(filename):
+    json_file = open('model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("Emotion_Voice_Detection_Model.h5")
+
+    X, sample_rate = librosa.load(filename, res_type='kaiser_fast', duration=2.5,
+                                  sr=22050 * 2, offset=0.5)
+    sample_rate = np.array(sample_rate)
+    mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=13), axis=0)
+
+    featurelive = mfccs
+    livedf2 = featurelive
+    livedf2 = pd.DataFrame(data=livedf2)
+    livedf2 = livedf2.stack().to_frame().T
+    twodim = np.expand_dims(livedf2, axis=2)
+    livepreds = loaded_model.predict(twodim, batch_size=32, verbose=1)
+    livepreds1 = livepreds.argmax(axis=1)
+    liveabc = livepreds1.astype(int).flatten()
+
+    return label[int(liveabc)]
 
 
 import operator
