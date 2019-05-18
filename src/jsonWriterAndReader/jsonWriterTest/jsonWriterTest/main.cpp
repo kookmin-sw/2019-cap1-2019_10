11a026dc9dc48566f133e51b7b3cd9b718527c1a
ddi04078 <970619abc@naver.com>
2019-05-18 21:56:09 +0900

    json Writer Test and libraries for this

---
+++ src/jsonWriterAndReader/jsonWriterTest/jsonWriterTest/main.cpp
@@ -0,0 +1,30 @@
+#pragma comment(lib, "lib_json.lib")
+#pragma warning(disable:4996)
+
+#include<iostream>
+#include<fstream>
+#include<memory>
+#include"json/json.h"
+
+using namespace std;
+using namespace Json;
+
+
+int main() {
+	ofstream json_file;
+	json_file.open("JSON_DATA.json");
+
+	Value Comp;
+	Comp["CPU"] = "I7";
+
+	StreamWriterBuilder builder;
+	builder["commentStyle"] = "None"; builder["indentation"] = "	";
+	unique_ptr<Json::StreamWriter> writer(builder.newStreamWriter());
+
+	writer->write(Comp, &cout);
+	writer->write(Comp, &json_file);
+	cout << endl;
+	json_file.close();
+
+	return 0;
+}
