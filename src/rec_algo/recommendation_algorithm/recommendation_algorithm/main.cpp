a7f1149b8f484e39e6e5e34c373a45c7353835bf
ddi04078 <970619abc@naver.com>
2019-05-23 22:10:27 +0900

    parsing a .json file with 8 raw emotions from face API

--- src/rec_algo/recommendation_algorithm/recommendation_algorithm/main.cpp
+++ src/rec_algo/recommendation_algorithm/recommendation_algorithm/main.cpp
@@ -6,6 +6,7 @@
 #include <cstring>
 #include <map>
 #include <functional> 
+#include <typeinfo>
 #include <memory>
 #include "json/json.h"
 
@@ -33,30 +34,41 @@ int main() {
 	JSONCPP_STRING errs;
 	bool ok = parseFromStream(builder, json_dir, &root, &errs);
 
-	multimap<double, string, greater<double>> emotion_res; //greater<int>면 key가 내림차순으로 정렬된다. 
+	//multimap : 중복키 허용 / 정렬됨 
+	multimap<double, string, greater<double>> tmp_res; //greater<int>면 key가 내림차순으로 정렬된다. 
 	
 	if (ok == true) {
-		//json파일에서 각 감정에 해당하는 비율을 가져와 double형의 변수에 넣는다. 
+		//json파일에서 각 감정에 해당하는 비율을 가져와 double형의 변수에 넣는다. (face API 결과임)
 		double anger = root.get("ANGER", 0.0).asDouble();
 		double fear = root.get("FEAR", 0.0).asDouble();
-		double contempt_disgust = root.get("CONTEMPT_DISGUST", 0.0).asDouble();
+		double fear = root.get("DISGUST", 0.0).asDouble();
+		double contempt_disgust = root.get("CONTEMPT", 0.0).asDouble();
+		double contempt_disgust = root.get("NEUTRAL", 0.0).asDouble();
 		double happiness = root.get("HAPPINESS", 0.0).asDouble();
 		double sad = root.get("SAD", 0.0).asDouble();
 		double surprise = root.get("SURPRISE", 0.0).asDouble();
 
-		//multimap인 emotion_res에 key-value 형태로 저장한다. key : 비율, value : 감정
-		emotion_res.insert(make_pair(anger, "ANGER"));
-		emotion_res.insert(make_pair(fear, "FEAR"));
-		emotion_res.insert(make_pair(contempt_disgust,"CONTEMPT_DISGUST"));
-		emotion_res.insert(make_pair(happiness, "HAPPINESS"));
-		emotion_res.insert(make_pair(sad, "SAD"));
-		emotion_res.insert(make_pair(surprise,"SURPRISE"));
+		//multimap인 tmp_res에 key-value 형태로 저장한다. key : 비율, value : 감정
+		tmp_res.insert(make_pair(anger, "ANGER"));
+		tmp_res.insert(make_pair(fear, "FEAR"));
+		tmp_res.insert(make_pair(contempt_disgust,"CONTEMPT_DISGUST"));
+		tmp_res.insert(make_pair(happiness, "HAPPINESS"));
+		tmp_res.insert(make_pair(sad, "SAD"));
+		tmp_res.insert(make_pair(surprise,"SURPRISE"));
 
-		//디버깅-내림차순 출력
-		for (auto &it : emotion_res) {
-			cout << it.first << " " << it.second << endl;
+		//tmp_res 에는 비율-감정 pair로 담겨있다. ( 0.6, 'FEAR' 등) 
+
+		//예외처리와 전처리를 하면서 6개의 감정으로 바꾸고, 이를 emotion_res에 저장한다.
+		map<double, string, greater<double> > emotion_res;
+
+	/*	map<double, string>::iterator iter;
+		for (iter = tmp_res.begin(); iter != tmp_res.end(); ++iter) {
+			cout << "(" << iter->first << "," << iter->second << ")" << " " << endl;
+			if (iter->first < 0.05) {
+
+			}
 		}
-
+	*/ 
 	}
 	else cout << "parse failed" << endl;
 
