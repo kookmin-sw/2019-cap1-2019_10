225bada3cd54d68ecaf4e20d7628e5b01f409231
강은채 <eunchae0280@naver.com>
2019-05-27 20:36:52 +0900

    if/esle about file.wav

--- src/Speech-Emotion-Model/Django&Model/code(final)/DjangoApp/views.py
+++ src/Speech-Emotion-Model/Django&Model/code(final)/DjangoApp/views.py
@@ -6,6 +6,7 @@ import librosa
 import numpy as np
 import pandas as pd
 import glob
+import os
 import tensorflow as tf
 from keras.models import model_from_json
 
@@ -25,15 +26,25 @@ class call(APIView):
 
         #과제!! 어플에서 byte array로 audiorecord file을 준다. 그것을 .wav로
         audio_file = request.FILES.get('audio','')
-        path = default_storage.save('file.wav', ContentFile(audio_file.read()))
+      
+        #파일의 이름이 중복될때 다른이름으로 저장되지 않도록 기존의 file.wav를 삭제
+        if os.path.isfile('./media/file.wav'):
+          os.remove('./media/file.wav')
+        else :
+            path = default_storage.save('file.wav', ContentFile(audio_file.read()))
         #file.wav로 저장
 
         #b = numpy.array(audio_file, dtype=numpy.int32)///시도했지만 계속 typeError가 났음
         #file= scipy.io.wavfile.write(r"C:\Users\eunch\source\repos\DjangoWebProject1\DjangoWebProject1\myfile.wav", 
         #44100, b)
         #어플에서 보내는 audio파일의 frequency가 44100
-
-        return HttpResponse(labelfrommodel('file.wav'))
+        json_file = open('model.json', 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        loaded_model = model_from_json(loaded_model_json)
+        # load weights into new model
+        loaded_model.load_weights("Emotion_Voice_Detection_Model.h5")
+        return HttpResponse(labelfrommodel('file.wav',loaded_model))
         
     
 lb = LabelEncoder()
@@ -52,14 +63,9 @@ label = [
     #label index값을 지키기위해 중복되게 둠
 ]
 
-def labelfrommodel(filename):
+def labelfrommodel(filename, load_model):
     
-    json_file = open('model.json', 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    loaded_model = model_from_json(loaded_model_json)
-    # load weights into new model
-    loaded_model.load_weights("Emotion_Voice_Detection_Model.h5")
+   
 
     X, sample_rate = librosa.load( filename, res_type='kaiser_fast', duration=2.5,
                                   sr=22050*2, offset=0.5)
@@ -71,7 +77,7 @@ def labelfrommodel(filename):
     livedf2 = pd.DataFrame(data=livedf2)
     livedf2 = livedf2.stack().to_frame().T
     twodim = np.expand_dims(livedf2, axis=2)
-    livepreds = loaded_model.predict(twodim, batch_size=32, verbose = 1)
+    livepreds = load_model.predict(twodim, batch_size=32, verbose = 1)
     livepreds1 = livepreds.argmax(axis=1)
     liveabc = livepreds1.astype(int).flatten()
     
