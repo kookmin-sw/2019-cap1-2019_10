a33c64ed2c4abbe0d64860912fcb4532a36931d1
wsubin <dnjstnqls@hanmail.net>
2019-05-29 01:06:36 +0900

    Merge branch 'database'

---
+++ src/pyDjango/mmm_project/my_mood_music/admin.py
@@ -0,0 +1,52 @@
+from django.contrib import admin
+from my_mood_music.models import Analysis_Result, Happiness, Anger, Fear, Surprise, Disgust, Sadness, Subclass_Sad, Lie, Child   
+
+# Register your models here.
+
+
+#Emotion 클래스가 Admin사이트에서 어떤 모습으로 보여줄지를 정의
+#class UserAdmin(admin.ModelAdmin):
+	#list_display = ('id_u','email','password', 'research')
+
+class ResultAdmin(admin.ModelAdmin):
+	list_display = ('id', 'user_id','music_r1', 'music_r2', 'music_r3')
+	
+class HappinessAdmin(admin.ModelAdmin):
+	list_display = ('id','music_h','age_h', 'link_h', 'tag_h1', 'tag_h2')
+
+class AngerAdmin(admin.ModelAdmin):
+	list_display = ('id','music_a','age_a', 'link_a', 'tag_a1', 'tag_a2')
+
+class FearAdmin(admin.ModelAdmin):
+	list_display = ('id','music_f','age_f', 'link_f', 'tag_f1', 'tag_f2')
+
+class SurpriseAdmin(admin.ModelAdmin):
+	list_display = ('id','music_su','age_su', 'link_su', 'tag_su1', 'tag_su2')
+
+class DisgustAdmin(admin.ModelAdmin):
+	list_display = ('id','music_d','age_d', 'link_d', 'tag_d1', 'tag_d2')
+
+class SadnessAdmin(admin.ModelAdmin):
+	list_display = ('id','music_s','age_s', 'link_s','subclass_s', 'tag_s1', 'tag_s2')
+
+class SubclassAdmin(admin.ModelAdmin):
+	list_display = ('id','subclass')
+
+class LieAdmin(admin.ModelAdmin):
+	list_display = ('id','music_l', 'link_l')
+
+class ChildAdmin(admin.ModelAdmin):
+	list_display = ('id','music_c', 'link_c')   
+	
+	
+#admin.site.register(User_Information,UserAdmin)
+admin.site.register(Analysis_Result,ResultAdmin)
+admin.site.register(Happiness,HappinessAdmin)
+admin.site.register(Anger,AngerAdmin)
+admin.site.register(Fear,FearAdmin)
+admin.site.register(Surprise,SurpriseAdmin)
+admin.site.register(Disgust,DisgustAdmin)
+admin.site.register(Sadness,SadnessAdmin)
+admin.site.register(Subclass_Sad,SubclassAdmin)
+admin.site.register(Lie, LieAdmin)
+admin.site.register(Child, ChildAdmin)
---
+++ src/pyDjango/mmm_project/my_mood_music/models.py
@@ -0,0 +1,160 @@
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+# Create your models here.
+
+@python_2_unicode_compatible
+		
+'''''
+class User_Information(models.Model):
+        id = models.IntegerField(primary_key=True)
+        user_id = models.CharField(max_length=300)
+        password = models.CharField(max_length=300)
+        research = models.IntegerField(default=0)
+	
+        def __str__(self):
+                return '{} {} {} {}'.format(self.id, self.user_id, self.password, self.research)
+
+
+class User_Information(models.Model):
+        user = models.OneToOneField(User, on_delete=models.CASCADE)
+        user_id = models.CharField(max_length=300)
+        password = models.CharField(max_length=300)
+        research = models.IntegerField(default=0)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+                Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()       
+
+               
+        
+ onetoonefield
+
+
+class User_Table(models.Model):
+        user_information = models.OneToOneField(
+                User_Information,
+                on_delete = models.CASCADE,
+                primary_key = True,
+        )
+        def __str__(self):
+                return '{}'.format(self.user_information)
+                
+''' 
+        
+class Analysis_Result(models.Model):
+        id = models.IntegerField(primary_key=True)
+        user_id = models.CharField(max_length=300, null=True)
+        music_r1 = models.CharField(max_length=500)
+        music_r2 = models.CharField(max_length=500)
+        music_r3 = models.CharField(max_length=500)
+
+        def __str__(self):
+                return '{} {} {} {} {}'.format(self.id, self.user_id, self.music_r1, self.music_r2, self.music_r3)
+        
+        
+class Happiness(models.Model):
+        id = models.IntegerField(primary_key=True)
+        music_h = models.CharField(max_length=500)
+        age_h = models.IntegerField(default=0)
+        link_h = models.URLField()
+        tag_h1 = models.CharField(max_length=500, null=True)
+        tag_h2 = models.CharField(max_length=500, null=True)
+
+        def __str__(self):
+                return '{} {} {} {} {} {}'.format(self.id, self.music_h, self.age_h, self.link_h, self.tag_h1, self.tag_h2)
+        
+               
+class Anger(models.Model):
+        id = models.IntegerField(primary_key=True)
+        music_a = models.CharField(max_length=500)
+        age_a = models.IntegerField(default=0)
+        link_a = models.URLField()
+        tag_a1 = models.CharField(max_length=500, null=True)
+        tag_a2 = models.CharField(max_length=500, null=True)
+        
+        def __str__(self):
+                return '{} {} {} {} {} {}'.format(self.id, self.music_a, self.age_a, self.link_a, self.tag_a1, self.tag_a2)
+
+                
+class Fear(models.Model):
+        id = models.IntegerField(primary_key=True)
+        music_f = models.CharField(max_length=500)
+        age_f = models.IntegerField(default=0)
+        link_f = models.URLField()
+        tag_f1 = models.CharField(max_length=500, null=True)
+        tag_f2 = models.CharField(max_length=500, null=True)
+        
+        def __str__(self):
+                return '{} {} {} {} {} {}'.format(self.id, self.music_f, self.age_f, self.link_f, self.tag_f1, self.tag_f2)                
+
+
+class Surprise(models.Model):
+        id = models.IntegerField(primary_key=True)
+        music_su = models.CharField(max_length=500)
+        age_su = models.IntegerField(default=0)
+        link_su = models.URLField()
+        tag_su1 = models.CharField(max_length=500, null=True)
+        tag_su2 = models.CharField(max_length=500, null=True)
+        
+        def __str__(self):
+                return '{} {} {} {} {} {}'.format(self.id, self.music_su, self.age_su, self.link_su, self.tag_su1, self.tag_su2)
+
+                
+class Disgust(models.Model):
+        id = models.IntegerField(primary_key=True)
+        music_d = models.CharField(max_length=500)
+        age_d = models.IntegerField(default=0)
+        link_d = models.URLField()
+        tag_d1 = models.CharField(max_length=500, null=True)
+        tag_d2 = models.CharField(max_length=500, null=True)
+        
+        def __str__(self):
+                return '{} {} {} {} {} {}'.format(self.id, self.music_d, self.age_d, self.link_d, self.tag_d1, self.tag_d2)
+
+                
+class Sadness(models.Model):
+        id = models.IntegerField(primary_key=True)
+        music_s = models.CharField(max_length=500)
+        age_s = models.IntegerField(default=0)
+        link_s = models.URLField()
+        subclass_s = models.IntegerField(Subclass_Sad, on_delete=models.CASCADE)
+        tag_s1 = models.CharField(max_length=500, null=True)
+        tag_s2 = models.CharField(max_length=500, null=True)
+        
+        def __str__(self):
+                return '{} {} {} {} {} {} {}'.format(self.id, self.music_s, self.age_s, self.link_s, self.subclass_s, self.tag_s1, self.tag_s2)
+
+                
+class Subclass_Sad(models.Model):
+        id = models.IntegerField(primary_key=True)
+        subclass = models.CharField(max_length=500)
+
+        def __str__(self):
+                return '{} {}'.format(self.id, self.subclass)
+
+
+class Lie(models.Model):
+        id = models.IntegerField(primary_key=True)
+        music_l = models.CharField(max_length=500)
+        link_l = models.URLField()
+
+        def __str__(self):
+                return '{} {} {}'.format(self.id, self.music_l, self.link_l)
+
+
+class Child(models.Model):
+        id = models.IntegerField(primary_key=True)
+        music_c = models.CharField(max_length=500)
+        link_c = models.URLField()
+
+        def __str__(self):
+                return '{} {} {}'.format(self.id, self.music_c, self.link_c)      
